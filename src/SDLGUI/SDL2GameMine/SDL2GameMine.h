bd5db888540489d6c4dfe6732b93b76914a38135
xingyun86 <xingyun_ppshuai@yeah.net>
2020-07-25 13:12:56 +0800

    提交新例子代码

---
+++ src/SDLGUI/SDL2GameMine/SDL2GameMine.h
@@ -0,0 +1,18 @@
+﻿// SDL2GameMine.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_net.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+#ifdef main
+#undef main
+#endif // main
+
+// TODO: Reference additional headers your program requires here.
